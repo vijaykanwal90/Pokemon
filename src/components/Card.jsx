957bcef54f4d1016c5800ec3dc236ba167b091b0
Vijay Singh Kanwal <vijaykanwal90@gmail.com>
2023-07-30 17:52:55 +0530

    api fetch working

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './style.css';
 
-const Card = () => {
+const Card = ({pokemon ,loading}) => {
+  console.log(pokemon);
   return (
     <>
-      <div className='cardItem'>
-        <h2>1</h2>
-        <img src="https://i.pinimg.com/236x/54/e7/50/54e750a0ca8d50dfd639a5c243e6d3fb--pokemon-blastoise-pokemon-go.jpg" alt="" />
-        <h2>Pikachu</h2>
+    {
+      loading ? <h1>Loading ....</h1>:
+      pokemon.map((item)=>{
+        return <>
+            <div className='cardItem'>
+        <h2>{item.id}</h2>
+        <img src={item.sprites.front_default} alt="" />
+        <h2>{item.name}</h2>
 
       </div>
+        </>
+      })
+    }
+  
     </>
   )
 }
--- src/components/MainInfo.jsx
+++ src/components/MainInfo.jsx
@@ -1,30 +1,57 @@
 import React from 'react'
 import Card from './Card';
 import './style.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import PokeInfo from './PokeInfo';
 const MainInfo = () => {
+  const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [nextUrl, setNextUrl] = useState();
+  const [prevUrl, setPrevUrl] = useState();
+  const pokeFun = async () => {
+    setLoading(true);
+    const res = await axios.get(url);
+    setNextUrl(res.data.next);
+    setPrevUrl(res.data.previous);
+    // console.log(res.data.results);
+    getPokemon(res.data.results);
+    setLoading(false);
+    // console.log(pokeData)
+  }
+  const getPokemon = async (res) => {
+    res.map(async (item) => {
+      const result = await axios.get(item.url)
+      // console.log(result.data);
+      setPokeData(state => {
+        state = [...state, result.data]
+        state.sort((a, b)=>a.id>b.id?1:-1)
+        return state;
+      })
+    })
+  }
+  useEffect(() => {
+    pokeFun();
+  }, [url])
+
+
   return (
     <>
       <div className="container">
         <div className="leftContent">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-
+          <Card pokemon={pokeData} loading={loading} />
+          <div className="buttons">
+            <button>Previous</button>
+            <button>Next</button>
+          </div>
         </div>
         <div className="rightContent">
-         <PokeInfo/>
-            
+          <PokeInfo />
+
         </div>
       </div>
-      <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
-      </div>
+
     </>
   )
 }
