50049cdf66e2aefdbeeec552b82e07da4317a575
Vijay Singh Kanwal <vijaykanwal90@gmail.com>
2023-08-17 17:11:02 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 import Navbar from './components/Navbar';
 import MainInfo from './components/MainInfo';
 import './components/style.css';
+import { BrowserRouter, Route, Router , Routes } from 'react-router-dom';
 // import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-     <Navbar/>
-    <MainInfo/>
-    </>
+    <Router>
+    
+    <Navbar/>
+    
+     
+     {/* <Routes>
+      <Route exact  path='/' element={<Home/>} </Route>
+      <Route exact  path='/marvel' element={<Marvel/>} </Route>
+     </Routes>
+     
+     <MainInfo/>
+     </Router> */}
+    <Routes>
+                <Route exact path='/' element={< Home />}></Route>
+                <Route exact path='/Pokemon' element={< Pokemon />}></Route>
+
+                <Route exact path='/marvel' element={< Marvel />}></Route>
+                <Route exact path='/about' element={< About />}></Route>
+        </Routes>
+     </>
    
-  );
+  )
 }
 
 export default App;
